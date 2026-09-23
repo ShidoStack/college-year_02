6a7fba4bc4f56144a1150f18aa4fe94d10b581c3
ShidoStack <2025.lokendraj@isu.ac.in>
2026-09-23 14:47:18 +0530

    update repo

--- year_02/sem_01/sprint_01/backend nodeJS_mongoDBandFirebase/assignment12.0/index.js
+++ year_02/sem_01/sprint_01/backend nodeJS_mongoDBandFirebase/assignment12.0/index.js
@@ -1,1 +1,31 @@
-console.log(619)
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+const User = require("./models/User");
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB Atlas connected successfully!");
+    })
+    .catch((error) => {
+        console.log("MongoDB connection error:", error);
+    });
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Authentication server is running"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
